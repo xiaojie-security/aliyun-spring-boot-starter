25b6fba6bb44d24acd43bc8a842f33063e7c0c3d
当夜晚来临 <2315912943@qq.com>
2026-07-16 10:19:00 +0800

    fix: 调整支付宝支付自动装配开关前缀

--- src/main/java/com/aliyun/config/AlipayPaymentConfiguration.java
+++ src/main/java/com/aliyun/config/AlipayPaymentConfiguration.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Bean;
 
 @AutoConfiguration
 @RequiredArgsConstructor
-@ConditionalOnProperty(prefix = "aliyun.pay.payment", name = "enable", havingValue = "true")
+@ConditionalOnProperty(prefix = "alipay.payment", name = "enable", havingValue = "true")
 public class AlipayPaymentConfiguration {
 
     /**
